0c069943c4f2df043f87669b071c888a290ac070
FIRST_NAME LAST_NAME <bondbond796@yahoo.com>
2020-10-06 20:06:20 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -242,8 +242,8 @@
     </footer>
   </div>
 
+  <script src="{{ asset('js/app.js') }}" defer></script>
 
-  <script src="{{ asset('js/app.js') }}" ></script>
   <script src="js/jquery-3.3.1.min.js"></script>
   <script src="js/jquery-ui.js"></script>
   <script src="js/popper.min.js"></script>
